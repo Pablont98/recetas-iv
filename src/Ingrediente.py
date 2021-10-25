29fdf919c31205f945d525a8060f351a88055dfa
Juan Ignacio Villegas Llano <vlljuan99@correo.ugr.es>
2021-10-25 19:33:37 +0200

    Añade los cambios pedidos en la clase Ingrediente

--- src/Ingrediente.py
+++ src/Ingrediente.py
@@ -1,9 +1,13 @@
 class Ingrediente:
     '''Clase que alberga los datos necesarios para guardar un ingrediente correctamente.'''
 
-    def __init__(self, nombre=None, descripcion=None, vitaminas=None, precio=None, kcals=None):
+    def __init__(self, nombre=None, descripcion=None, vitaminas=None, precio=None, kcals_base=None, unidad=None, cantidad=None):
         self.nombre = nombre
         self.descripcion = descripcion
         self.vitaminas = vitaminas
         self.precio = precio
-        self.kcals = kcals
+        self.kcals_base = kcals_base
+        self.unidad = unidad
+
+    def kcals_totales():
+        pass
