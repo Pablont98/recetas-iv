e140c5e69292e41aa44c0c167d32514b257cafac
Pablo Nuñez Tejero <90459270+Pablont98@users.noreply.github.com>
2021-11-05 18:14:35 +0100

    Add files via upload

--- tasks.py
+++ tasks.py
@@ -1,24 +1,37 @@
 from invoke import task, run
 
-@task(name = "installdeps") 
-def installdeps(c):
-	"""
-	Instala las dependencias para que la aplicacion funcione
-	"""
 
-	print("Instalando dependencias:")
-	run("pip install -r requirements.txt")
-	
+@task(name="install")
+def install(c):
+    """
+    Instala las clases
+    """
+
+    # installdeps(c)
+    run("python3 src/*.py")
+
+
+@task(name="installdeps")
+def installdeps(c):
+    """
+    Instala las dependencias para que la aplicacion funcione
+    """
+
+    print("Instalando dependencias:")
+    run("pip install -r requirements.txt")
+
+
 @task
 def test(c):
-	"""
-	Para comprobar que funciona correctamente
-	"""
-	print("Sin comprobaciones por el momento")
-	
-@task 
+    """
+    Para comprobar que funciona correctamente
+    """
+    run("python3 -m pytest")
+
+
+@task
 def check(c):
-	"""
-	Para comprobar que la sintaxis es correcta
-	"""
-	run("pylint --errors-only src")
+    """
+    Para comprobar que la sintaxis es correcta
+    """
+    run("pylint --errors-only src")
