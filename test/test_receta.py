701e714c8b07543b9560a3e344245798ce24e316
Pablo Nuñez Tejero <90459270+Pablont98@users.noreply.github.com>
2021-11-06 10:43:48 +0100

    Corregidos errores sintácticos de test_receta.py

--- test/test_receta.py
+++ test/test_receta.py
@@ -12,14 +12,14 @@ def receta():
     ingrediente_dos = Ingrediente('Cebolla', '', 'A,B6,C,E', 1, 40, 'gramos', 250)
     ingrediente_tres = Ingrediente('Pimiento', '', 'E,C', 0.90, 20, 'gramos', 500)
     ingrediente_cuatro = Ingrediente('Sal', '', '', 0.50, 0, 'gramos', 50)
-    ingrediente_cinco = Ingrediente('Aceite', '', 'A,D,E,K',0.50, 2, 'mililitro', 350)
+    ingrediente_cinco = Ingrediente('Aceite', '', 'A,D,E,K',0.50, 2, 'mililitros', 350)
     lista_ingredientes = list()
     lista_ingredientes.append(ingrediente_uno)
     lista_ingredientes.append(ingrediente_dos)
     lista_ingredientes.append(ingrediente_tres)
     lista_ingredientes.append(ingrediente_cuatro)
     lista_ingredientes.append(ingrediente_cinco)
-    elaboracion = "Cortar las patatas, las cebollas y los pimientos, lavar en agua, echar sal, echar el aceite" \
+    elaboracion = "Cortar las patatas, las cebollas y los pimientos, lavar en agua, echar sal, echar el aceite " \
                    "en una sarten calentar y cuando el aceite este caliente freir las patatas, las cebollas y el pimiento"
     receta = Receta('Salteado de patatas', lista_ingredientes, 45, 'media',elaboracion)
 
