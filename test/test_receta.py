35f51aafd1cf9f09d170c87744dea5d2d511fe65
Pablo Nuñez Tejero <e.pablonueztejer@go.ugr.es>
2021-11-12 11:31:11 +0100

    Corregidos import y funciones test
    Se han adecuado los tests a la nueva organización del código

--- test/test_receta.py
+++ test/test_receta.py
@@ -1,35 +1,35 @@
 # Archivo para realizar tests a la clase Receta de nuestro proyecto
 
 import pytest
-import os
 from src.Receta import *
 from src.Ingrediente import *
 
 
 @pytest.fixture
 def receta():
-    ingrediente_uno = Ingrediente('Patata', '', 'B,C', 0.90, 80, 'gramos', 1000)
-    ingrediente_dos = Ingrediente('Cebolla', '', 'A,B6,C,E', 1, 40, 'gramos', 250)
-    ingrediente_tres = Ingrediente('Pimiento', '', 'E,C', 0.90, 20, 'gramos', 500)
-    ingrediente_cuatro = Ingrediente('Sal', '', '', 0.50, 0, 'gramos', 50)
-    ingrediente_cinco = Ingrediente('Aceite', '', 'A,D,E,K',0.50, 2, 'mililitros', 350)
+    ingrediente_uno = Ingrediente('Patata', '', 'B,C', 0.90, 80, 2.2, 16.1, 0.2)
+    ingrediente_dos = Ingrediente('Cebolla', '', 'A,B6,C,E', 1, 32, 1.1, 5.3, 0.2)
+    ingrediente_tres = Ingrediente('Pimiento verde', '', 'E,C', 0.90, 20, 0.6, 1.6, 0.8)
+    ingrediente_cuatro = Ingrediente('Sal', '', '', 0.50, 0, 0, 0, 0)
+    ingrediente_cinco = Ingrediente('Aceite de oliva', '', 'A,D,E,K',2.50, 899, 0, 0, 99.9)
     lista_ingredientes = list()
     lista_ingredientes.append(ingrediente_uno)
     lista_ingredientes.append(ingrediente_dos)
     lista_ingredientes.append(ingrediente_tres)
     lista_ingredientes.append(ingrediente_cuatro)
     lista_ingredientes.append(ingrediente_cinco)
+    lista_cantidades = [1000,250,500,10,350]
     elaboracion = "Cortar las patatas, las cebollas y los pimientos, lavar en agua, echar sal, echar el aceite " \
                    "en una sarten calentar y cuando el aceite este caliente freir las patatas, las cebollas y el pimiento"
-    receta = Receta('Salteado de patatas', lista_ingredientes, 45, 2, elaboracion)
+    receta = Receta('Salteado de patatas', lista_ingredientes, lista_cantidades, 45, 'media',elaboracion)
 
     return receta
 
 def test_kcals_ingrediente_max(receta):
-    assert receta.ingredientes[0].kcals_totales() == 800
+    assert receta.ingredientes[0].kcals_totales(receta.cantidades[0]) == 800
 
 def test_kcals_max_receta(receta):
-    assert receta.kcals_maximas() == 1294.65
+    assert receta.kcals_maximas() == 4126.5
 
 def test_proteinas_max_receta(receta):
     assert receta.proteinas_maximas() == 27.75
@@ -38,5 +38,5 @@ def test_hidratos_max_receta(receta):
     assert receta.hidratos_maximos() == 182.25
 
 def test_grasas_max_receta(receta):
-    assert receta.grasas_maximas() == 356.8
+    assert receta.grasas_maximas() == 356.15
 
