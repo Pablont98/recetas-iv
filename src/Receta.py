a4db18f6c01186b8cf3e0314154d22b708777580
Pablo Nuñez Tejero <e.pablonueztejer@go.ugr.es>
2021-11-12 11:29:48 +0100

    Corregidas funcionalidades receta.py
    Se ha reorganizado el código para adaptarlo a las necesidades que se buscan

--- src/Receta.py
+++ src/Receta.py
@@ -1,56 +1,65 @@
-#from Ingrediente import *
-
 class Receta:
     '''Clase que alberga los datos necesarios para guardar una receta correctamente.'''
 
-    def __init__(self, nombre, ingredientes, duracion, dificultad, elaboracion):
+    def __init__(self, nombre, ingredientes, cantidades, duracion, dificultad, elaboracion):
         self.nombre = nombre
         self.ingredientes = list(ingredientes)
+        self.cantidades = list(cantidades)
         self.duracion = duracion
         self.dificultad = dificultad
         self.elaboracion = elaboracion
+        self.carta_receta = ''
 
     def kcals_maximas(self):
-        ''' Método que calcula el aporte calórico total de la receta a partir de sus ingredientes.'''
+        ''' Método que calcula el aporte calórico total de la receta a partir de sus ingredientes.
+            En este momento no se tendrá en cuenta la elaboración de la receta, ya que esta
+            puede influir en el numero de calorias.'''
         kcals_max = 0
+        contador = 0
         for ing in self.ingredientes:
-            if ((ing.nombre.find("Aceite") >= 0) and (ing.cantidad > 10)):
-                kcals_max += ((10 * ing.kcals_totales())/100)
-            else:
-                kcals_max += ing.kcals_totales()
+            kcals_max += ing.kcals_totales(self.cantidades[contador])
+            contador += 1
         return round(kcals_max,2)
 
     def hidratos_maximos(self):
         ''' Método que calcula el número de hidratos de la receta a partir de sus ingredientes.'''
         hidratos_max = 0
+        contador = 0
         for ing in self.ingredientes:
-            hidratos_max += ing.hidratos_totales()
+            hidratos_max += ing.hidratos_totales(self.cantidades[contador])
+            contador += 1
         return round(hidratos_max,2)
 
     def proteinas_maximas(self):
         ''' Método que calcula el número de proteinas de la receta a partir de sus ingredientes.'''
         proteinas_max = 0
+        contador = 0
         for ing in self.ingredientes:
-            proteinas_max += ing.proteinas_totales()
+            proteinas_max += ing.proteinas_totales(self.cantidades[contador])
+            contador += 1
         return round(proteinas_max,2)
 
     def grasas_maximas(self):
-        ''' Método que calcula las grasas totales de la receta a partir de sus ingredientes.'''
+        ''' Método que calcula las grasas totales de la receta a partir de sus ingredientes.
+            En este momento no se tendrá en cuenta la elaboración de la receta, ya que esta
+            puede influir en el numero de grasas.'''
         grasas_max = 0
+        contador = 0
         for ing in self.ingredientes:
-            if (ing.nombre.find('Aceite')) and ing.cantidad > 10:
-                grasas_max += (10 * ing.grasas_totales())/100
-            grasas_max += ing.grasas_totales()
+            grasas_max += ing.grasas_totales(self.cantidades[contador])
+            contador += 1
         return round(grasas_max,2)
 
     def visualizar_receta(self):
         '''Método que muestra una carta mostrando informacion referente a la receta'''
         string_ingredientes = ""
+        contador = 0
         for ingrediente in self.ingredientes:
-            string_ingredientes += ingrediente.nombre + " cantidad: " + str(ingrediente.cantidad) + " " + ingrediente.unidad + "\n"
+            string_ingredientes += ingrediente.nombre + " cantidad: " + str(self.cantidades[contador]) + " gramos\n"
+            contador += 1
 
         string_dura_dificult = "\nDuracion: "+str(self.duracion)+" mins\n"+" Dificultad: "+self.dificultad
         string_elaboracion = "\nElaboracion \n" + self.elaboracion
-        carta_receta = (self.nombre + "\n\n" + string_ingredientes + string_dura_dificult + string_elaboracion)
+        self.carta_receta = (self.nombre + "\n\n" + string_ingredientes + string_dura_dificult + string_elaboracion)
 
-        print(carta_receta)
+        print(self.carta_receta)
