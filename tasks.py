06ea592e65e040451072c18016295ae54819586e
Pablo Nuñez Tejero <90459270+Pablont98@users.noreply.github.com>
2021-11-19 18:07:52 +0100

    Add files via upload

---
+++ tasks.py
@@ -0,0 +1,21 @@
+from invoke import task, run
+
+@task(name="installdeps")
+def installdeps(c):
+    """
+    Instala las dependencias para que la aplicacion funcione
+    """
+
+    print("Instalando dependencias:")
+    run("pip install pytest \
+        && pip install poetry \
+        && poetry update \
+        && poetry install")
+
+
+@task
+def test(c):
+    """
+    Para comprobar que funciona correctamente
+    """
+    run("python3 -m pytest")
